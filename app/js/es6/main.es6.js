859a15139c4a4bddbab6c31461e13fbf361a9d64
Matt Spell <matthew.spell1@gmail.com>
2014-05-02 17:39:45 -0500

    fully functional

---
+++ app/js/es6/main.es6.js
@@ -0,0 +1,47 @@
+/*global Pet, pets*/
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    $('#add').click(add);
+    $('#pets').on('click', '.eat', eat);
+    $('#pets').on('click', '.sleep', sleep);
+    $('#pets').on('click', '.play', play);
+  }
+
+  function eat(){
+    let name = $(this).closest('.pet').data('name');
+    let pet = Pet.find(name);
+    pet.eat();
+    pet.update();
+  }
+
+  function sleep(){
+    let name = $(this).closest('.pet').data('name');
+    let pet = Pet.find(name);
+    pet.sleep();
+    pet.update();
+  }
+
+  function play(){
+    let name = $(this).closest('.pet').data('name');
+    let pet = Pet.find(name);
+    pet.play();
+    pet.update();
+  }
+
+  function add(){
+    var gender = $('#gender').val();
+    var species = $('#species option:selected').text();
+    var speciesImg = $('#species').val();
+    var name = $('#name').val() || undefined;
+    var age = $('#age').val() || undefined;
+
+    let pet = new Pet(species, speciesImg, gender, name, age);
+    pets.push(pet);
+    pet.render();
+  }
+
+})();
---
+++ app/js/es6/pet.es6.js
@@ -0,0 +1,60 @@
+/*jshint unused:false*/
+/*exported Pet*/
+/*global _, pets*/
+
+class Pet{
+  constructor(species, speciesImg, gender, name='Cutey', age=3){
+    this.species = species;
+    this.speciesImg = `../media/${speciesImg}`;
+    this.gender = gender;
+    this.name = name;
+    this.age = age * 1;
+
+    this.health =_.random(10, 100);
+    this.full =_.random(5, 50);
+    this.mood =_.random(1, 10);
+  }
+
+  static find(name){
+    return _(pets).find(p=>p.name === name);
+  }
+
+  eat(){
+    this.full += _.random(1, 3);
+    if(this.full >= 100){this.full = 100;}
+  }
+
+  sleep(){
+    this.health += _.random(1, 3);
+    if(this.health >= 100){this.health = 100;}
+  }
+
+  play(){
+    this.mood += _.random(0, 1);
+    this.full -= _.random(1, 4);
+    this.health -= _.random(1, 4);
+    if(this.mood >= 100){this.mood = 100;}
+    if(this.health <= 0){$(`div[data-name=${this.name}]`).find('div.image').append('<div class="dead"></div>');}
+
+  }
+
+  update(){
+    $(`div[data-name=${this.name}]`).find('.full').css('width', `${this.full}%`);
+    $(`div[data-name=${this.name}]`).find('.health').css('width', `${this.health}%`);
+    $(`div[data-name=${this.name}]`).find('.mood').css('width', `${this.mood}%`);
+  }
+
+  render(){
+    $('#pets').append(`<div data-name=${this.name} class=pet>
+                        <div class=image style='background-image:url("${this.speciesImg}")'></div>
+                        <div class=nameSpec><span>Name: ${this.name}</span></div>
+                        <div class=ageGen><span>Age: ${this.age} | </span><span>Gender: ${this.gender}</span></div>
+                        <div class='meter'><span class='health' style='width:${this.health}%'>Health</span></div>
+                        <div class='meter'><span class='mood' style='width:${this.mood}%'><span class='words'>Mood</span></span></div>
+                        <div class='meter'><span class='full' style='width:${this.full}%'>Belly</span></div>
+                        <button class='eat'>Eat</button>
+                        <button class='sleep'>Sleep</button>
+                        <button class='play'>Play</button>
+                        </div>`);
+  }
+}
